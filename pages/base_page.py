af446a3a7760d38ef32d01e63e661dce1226b854
UP240244 <up240244@alumnos.upa.edu.mx>
2026-07-08 15:00:38 -0600

    feat(qa): implementar clase BasePage con esperas explícitas para el patrón POM

---
+++ pages/base_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        # Tiempo de espera por defecto de 10 segundos
+        self.wait = WebDriverWait(driver, 10)
+
+    def find_element(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def click_element(self, locator):
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        element.click()
+
+    def type_text(self, locator, text):
+        element = self.find_element(locator)
+        element.clear()
+        element.send_keys(text)
+        
+    def get_element_text(self, locator):
+        return self.find_element(locator).text
