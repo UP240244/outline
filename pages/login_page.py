e536168f41f49ce94b3316f1bea873474f31d199
UP240244 <up240244@alumnos.upa.edu.mx>
2026-07-08 17:58:18 -0600

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,8 +5,12 @@ class LoginPage(BasePage):
     USERNAME_FIELD = (By.ID, "username")
     PASSWORD_FIELD = (By.ID, "password")
     LOGIN_BUTTON = (By.XPATH, "//button[@type='submit']")
+    ERROR_BANNER = (By.CLASS_NAME, "error-banner")
 
     def login(self, user, pwd):
         self.type_text(self.USERNAME_FIELD, user)
         self.type_text(self.PASSWORD_FIELD, pwd)
         self.click_element(self.LOGIN_BUTTON)
+
+    def obtener_mensaje_error(self):
+        return self.get_element_text(self.ERROR_BANNER)
