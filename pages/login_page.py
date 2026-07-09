431d59561f6fa7f2b57b4667fbf6da22f6ea292f
UP240244 <up240244@alumnos.upa.edu.mx>
2026-07-08 18:11:08 -0600

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,16 +1,17 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 class LoginPage(BasePage):
     USERNAME_FIELD = (By.ID, "username")
     PASSWORD_FIELD = (By.ID, "password")
     LOGIN_BUTTON = (By.XPATH, "//button[@type='submit']")
-    ERROR_BANNER = (By.CLASS_NAME, "error-banner")
+    ERROR_MESSAGE = (By.CLASS_NAME, "error-banner")
 
     def login(self, user, pwd):
         self.type_text(self.USERNAME_FIELD, user)
         self.type_text(self.PASSWORD_FIELD, pwd)
         self.click_element(self.LOGIN_BUTTON)
 
-    def obtener_mensaje_error(self):
-        return self.get_element_text(self.ERROR_BANNER)
+    def obtener_error(self):
+        return self.find_element(self.ERROR_MESSAGE).text
